632e2f8a431424e96fb2032db6ce9ccad3288eef
Abdullah Al Rafee <58068580+Rafee1100@users.noreply.github.com>
2021-06-21 01:29:18 +0600

    backend done almost

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 const express = require('express')
-const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const ObjectID = require('mongodb').ObjectID
+const port = 8000
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.buztu.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
-app.get("/",(req,res)=>{
-    res.send("Thank you for using express")
-})
+const app = express();
+app.use(express.json())
+app.use(cors())
 
-app.listen(8000, ()=>{
-    console.log("Listening")
-})
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log(err)
+  const jobCollection = client.db("JobLagbe").collection("Jobs");
+  const employerCollection = client.db("JobLagbe").collection("employer");
+  const seekerCollection = client.db("JobLagbe").collection("seeker");
+  const adminCollection = client.db("JobLagbe").collection("admin");
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.get('/jobs', (req, res) => {
+    jobCollection.find()
+      .toArray((err, items) => {
+        res.send(items)
+      })
+  })
+  app.post('/isadmin', (req, res) => {
+    const email = req.body.email;
+    adminCollection.find({ email: email })
+      .toArray((err, admin) => {
+        res.send(admin.length > 0);
+      })
+  })
+
+  app.post('/addJob', (req, res) => {
+    const newJobData = req.body;
+    jobCollection.insertOne(newJobData)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+  })
+
+
+});
+
+
+app.listen(process.env.PORT || port)
